cd48a6aad7c8b9996d730eabdb56252ac3baf6a3
Renat <52@gmail.com>
2025-11-12 14:10:41 +0500

    init commit, закинул файлы обучения, теста и с весами модели

---
+++ cars_body_type_classification_test.py
@@ -0,0 +1,143 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from PIL import Image
+import numpy as np
+import requests
+from PIL import Image
+from io import BytesIO
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNetCustom(nn.Module):
+    def __init__(self, num_classes=8, dropout_p=0.3):
+        super(ResNetCustom, self).__init__()
+
+        self.init_conv = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        )
+
+        self.layer1 = self._make_layer(64, 64, num_blocks=2, stride=1)
+        self.layer2 = self._make_layer(64, 128, num_blocks=2, stride=2)
+        self.layer3 = self._make_layer(128, 256, num_blocks=2, stride=2)
+        self.layer4 = self._make_layer(256, 512, num_blocks=2, stride=2)
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.dropout = nn.Dropout(p=dropout_p)
+
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, in_channels, out_channels, num_blocks, stride):
+        layers = [ResidualBlock(in_channels, out_channels, stride)]
+        for _ in range(1, num_blocks):
+            layers.append(ResidualBlock(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.init_conv(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.avg_pool(out)
+        out = torch.flatten(out, 1)
+        out = self.dropout(out)
+        out = self.fc(out)
+        return out
+
+
+device = torch.device("cpu")
+num_classes = 7
+model = ResNetCustom(num_classes=num_classes)
+model.load_state_dict(torch.load("best_resnet_model_actual.pth", map_location=device))
+model.to(device)
+model.eval()
+
+print("Модель загружена и готова к тестированию")
+
+transform = transforms.Compose([
+    transforms.Resize((550, 550)),
+    transforms.CenterCrop((512, 512)),
+    transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
+    transforms.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 1.0)),
+    transforms.RandomAdjustSharpness(sharpness_factor=1.5, p=0.5),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5),
+                         (0.5, 0.5, 0.5))
+])
+
+def predict_with_crop_tta(image_path, model, class_names, device, n_crops=100):
+    image = Image.open(image_path).convert("RGB")
+    w, h = image.size
+    preds = []
+
+    crop_ratios = np.linspace(0.0, 0.2, n_crops)
+
+    for r in crop_ratios:
+        left = 0
+        # left = int(w * r)
+        top = int(h * r)
+        right = int(w)
+        # right = int(w * (1 - r))
+        bottom = int(h * (1 - r))
+
+        cropped = image.crop((left, top, right, bottom))
+        img_t = transform(cropped).unsqueeze(0).to(device)
+
+        with torch.no_grad():
+            outputs = model(img_t)
+            preds.append(outputs)
+
+    avg_pred = torch.stack(preds).mean(0)
+    print(class_names)
+    print(avg_pred)
+    class_index = avg_pred.argmax().item()
+
+    print(f"Финальное предсказание (усреднённое по {n_crops} кропам): {class_names[class_index]}")
+    plt.imshow(image)
+    plt.axis("off")
+    plt.title(f"Предсказано: {class_names[class_index]}")
+    plt.show()
+
+    return class_names[class_index]
+
+url = "https://favorit-motors.ru/upload/medialibrary/dee/kupe_2.jpg"
+response = requests.get(url)
+image = Image.open(BytesIO(response.content)).convert("RGB")
+
+image.save("test_car.jpg")
+
+class_names = ['Convertible', 'Coupe', 'Hatchback', 'Pick-Up', 'SUV', 'Sedan', 'VAN']
+predict_with_crop_tta("test_car.jpg", model, class_names, device)
---
+++ cars_body_type_classification_training.py
@@ -0,0 +1,151 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class ResNetCustom(nn.Module):
+    def __init__(self, num_classes=7, dropout_p=0.3):
+        super(ResNetCustom, self).__init__()
+
+        self.init_conv = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=7, stride=1, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=1, padding=1)
+        )
+
+        self.layer1 = self._make_layer(64, 64, num_blocks=2, stride=1)
+        self.layer2 = self._make_layer(64, 128, num_blocks=2, stride=2)
+        self.layer3 = self._make_layer(128, 256, num_blocks=2, stride=2)
+        self.layer4 = self._make_layer(256, 512, num_blocks=2, stride=2)
+        self.layer5 = self._make_layer(512, 512, num_blocks=2, stride=2)
+
+        self.avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.dropout = nn.Dropout(p=dropout_p)
+
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, in_channels, out_channels, num_blocks, stride):
+        layers = [ResidualBlock(in_channels, out_channels, stride)]
+        for _ in range(1, num_blocks):
+            layers.append(ResidualBlock(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.init_conv(x)
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = self.layer5(out)
+        out = self.avg_pool(out)
+        out = torch.flatten(out, 1)
+        out = self.dropout(out)
+        out = self.fc(out)
+        return out
+
+data_dir = '/content/drive/MyDrive/Cars_Body_Type'
+
+train_transform = transforms.Compose([
+    transforms.Resize((512, 512)),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomRotation(degrees=15),
+    transforms.ColorJitter(brightness=0.3, contrast=0.3, saturation=0.3, hue=0.1),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5),
+                         (0.5, 0.5, 0.5))
+])
+
+val_transform = transforms.Compose([
+    transforms.Resize((512, 512)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5),
+                         (0.5, 0.5, 0.5))
+])
+
+train_dataset = datasets.ImageFolder(os.path.join(data_dir, 'train'), transform=train_transform)
+val_dataset = datasets.ImageFolder(os.path.join(data_dir, 'valid'), transform=val_transform)
+
+train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_workers=2)
+val_loader = DataLoader(val_dataset, batch_size=16, shuffle=False, num_workers=2)
+
+num_classes = len(train_dataset.classes)
+print("Классов найдено:", num_classes)
+print("Типы кузова:", train_dataset.classes)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = ResNetCustom(num_classes=num_classes).to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+best_val_acc = 0.0
+num_epochs = 27
+
+best_model = None
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for images, labels in tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}"):
+        images, labels = images.to(device), labels.to(device)
+
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+    avg_train_loss = running_loss / len(train_loader)
+
+    model.eval()
+    correct, total = 0, 0
+    with torch.no_grad():
+        for images, labels in val_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    val_acc = 100 * correct / total
+    print(f"Epoch [{epoch+1}/{num_epochs}] | Train Loss: {avg_train_loss:.4f} | Val Acc: {val_acc:.2f}%")
+
+    if val_acc > best_val_acc:
+        best_val_acc = val_acc
+        best_model = model.state_dict()
+        torch.save(model.state_dict(), "best_resnet_model.pth")
+        print("Обнаружена новая лучшая модель")
+print("Сохранена лучшая модель")
+torch.save(best_model, "/content/drive/MyDrive/best_resnet_model.pth")
